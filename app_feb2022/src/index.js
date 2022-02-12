c11d422f7f0d649cd75fec292652654df45c58db
ruedalibre <andresruedalibre@gmail.com>
2022-02-11 22:09:10 -0500

    update feb 11

--- app_feb2022/src/index.js
+++ app_feb2022/src/index.js
@@ -1,17 +1,41 @@
+// IMPORTACIONES INICIALES
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const nombre = "Ruedalibre";
+const color = 'blue';
+const sesion = true;
+/* Los elementos JSX pueden almacenar código 
+javascript y a partir de esto puedo crear 
+plantillas para trabajar de una forma muy 
+dinámica en el dieseño de la aplicación */
+const JSX = (
+  /* Los div se pueden se pueden sintetizar como "fragmentos"
+  con la notación <> </> y cumplen la misma función que un div*/
+  <>
+    {/* Las clases en JSX se denotan como className.
+    Puedo asignar estilos en la misma línea del elemento
+    pasando los atributos entre corchetes. El primer 
+    corchete me permite acceder al elemento, y el segundo
+    a los atributos que le quiero dar. */}
+    {/* Para poder reutilizar atributos, puedo guardarlos
+    en una variable y después llamo el nombre de la variable
+    en vez de valor como tal */}
+    <h1 className='titulo' style={{color: color}} >¡Bienvenido {nombre}!</h1>
+    <p>Proyecto - febrero 2022</p>
+  </>
 );
+{/* CONDICIONALES: Ejemplo de inicio de sesión con una FUNCIÓN FLECHA*/}
+const verificarSesion = (sesion) => {
+  if(sesion === true) {
+    return JSX;
+  } else {
+    return <h1>No has iniciado sesión</h1>
+  }
+} 
+/* Esta es la forma como React pinta los elementos, indicando el contenedor 
+'root' (tipo div) donde va a ir ubicado todo el contenido de la aplicación */
+ReactDOM.render(verificarSesion(sesion), document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
