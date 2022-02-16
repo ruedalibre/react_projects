fac31e6f4e41e9c878faf013081ff72f3f7d71f7
ruedalibre <andresruedalibre@gmail.com>
2022-02-16 16:02:57 -0500

    update react app feb 16

--- projects/check_list_app/src/App.js
+++ projects/check_list_app/src/App.js
@@ -1,13 +1,32 @@
+import React, {useState} from 'react';
 import './App.css';
 import Header from './components.js/Header';
+import FormularioTareas from './components.js/FormularioTareas';
 
 /* Se cambió "function" a "const" para trabajar
 con el mismo método en todos los archivos */
 const App = () => {
+  const [tareas, cambiarTareas] = useState(
+    [
+      /* Construcción de objetos tarea dentro 
+      de un arreglo especificando id, tarea y su estado */
+      {
+        id: 1,
+        texto: 'Lavar la ropa',
+        completada: false
+      },
+      {
+        id: 2,
+        texto: 'Grabar tutorial',
+        completada: false
+      }
+    ]
+  ); 
+
   return (
     <div className="contenedor">
       <Header/>
-      
+      <FormularioTareas/>
     </div>
   );
 }
