68474cf07e559179491845b0e86bc27b115fa775
ruedalibre <andresruedalibre@gmail.com>
2022-02-15 23:04:11 -0500

    setting the new project

---
+++ projects/check_list_app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+/* Se cambió "function" a "const" para trabajar
+con el mismo método en todos los archivos */
+const App = () => {
+  return (
+    <div>
+      <h1>¡Bienvenido!</h1>
+    </div>
+  );
+}
+
+export default App;
