2a2ae5c243716323891cb315a728c9f57359fd2c
ruedalibre <andresruedalibre@gmail.com>
2022-02-18 11:45:27 -0500

    update blog-añpp react app part I

---
+++ projects/blog_app/src/componentes/Blog.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Blog = () => {
+    return(
+        <div>
+            <h2>Blog</h2>
+            <ul>
+                <li>Artículo #1</li>
+                <li>Artículo #2</li>
+                <li>Artículo #3</li>
+            </ul>
+      </div>
+    );
+};
+
+export default Blog;
