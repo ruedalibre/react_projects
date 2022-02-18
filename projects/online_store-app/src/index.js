acb1d5281e86f501c46aa6670d6bd7c6ea20b2f9
ruedalibre <andresruedalibre@gmail.com>
2022-02-18 18:05:31 -0500

    css styles online store react app

--- projects/online_store-app/src/index.js
+++ projects/online_store-app/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
+  /* Para que todas las rutas empiecen a ser 
+    adminstradas por BrowserRouter, debo encapsular
+    App en su interior  */
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
