91b0f8936f230cc3ccdf9e54246206e508872731
ruedalibre <andresruedalibre@gmail.com>
2022-02-23 23:01:46 -0500

    initial config for new firebase project

---
+++ projects/app_firebase/src/firebase/firebaseConfig.js
@@ -0,0 +1,31 @@
+/* ESTE ARCHIVO Y SU CARPETA NO VIENEN INCLUIDOS POR DEFECTO, 
+HAY QUE CREARLOS Y EL SCRIPT ESTA EN LA PAGINA DE FIREBASE, 
+DEBAJO DEL npm install firebase */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+/* Esta es una de las utilidades disponibles en la página de 
+Firebase. De allí se copian y se pegan aquí */
+import { getFirestore } from "firebase/firestore"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+/* ESTE OBJETO CONTINE LA CONFIGURACION DEL PROJECTO EN FIREBASE */
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+export default db;
+/*const */
+
