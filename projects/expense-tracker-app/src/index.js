2b7d9a738519aee06920d5425d6e6df7ebc8b9fa
ruedalibre <andresruedalibre@gmail.com>
2022-02-26 13:27:38 -0500

    initial config for new app

--- projects/expense-tracker-app/src/index.js
+++ projects/expense-tracker-app/src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import WebFont from 'webfontloader';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/* La configuración por defecto fue cambiada, para mayor limpieza, 
+por la siguiente: */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* Este código es para trabajar las fuentes de Google con Webfontloader */
+
+WebFont.load({
+  google: {
+    families: ['Work Sans:400,500,700', 'sans-serif']
+  }
+});
+
+const Index = () => {
+  return ( 
+    <App/>
+   );
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
