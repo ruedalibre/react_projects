e1b3ac02216ede59b649ac2c350acd552debe93b
ruedalibre <andresruedalibre@gmail.com>
2022-02-27 21:16:51 -0500

    formular styles, inputs and buttons

--- projects/expense-tracker-app/src/componentes/RegistroUsuarios.js
+++ projects/expense-tracker-app/src/componentes/RegistroUsuarios.js
@@ -1,8 +1,58 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import {Header, Titulo, ContenedorHeader, ContenedorBotones} from './../elementos/Header';
+import Boton from './../elementos/Boton';
+import { Formulario, Input, ContenedorBoton} from './../elementos/ElementosDeFormulario';
+import {ReactComponent as SvgLogin} from './../imagenes/registro.svg';
+import styled from "styled-components";
+
+const Svg = styled(SvgLogin)`
+    width: 100%;
+    max-height: 6.25rem; /* 100px */
+    margin-button: 1.25rem; /* 20px */
+`;
 
 const RegistroUsuarios = () => {
     return ( 
-        <h1>Registro de usuarios</h1>
+        <>
+            <Helmet>
+                <title>Expense Tracker</title>
+            </Helmet>
+
+            <Header>
+                {/* Debo implementar el contenedor Header porque contiene valores de media que se van a necesitar */}
+                <ContenedorHeader>
+                    <Titulo>Crear Cuenta</Titulo>
+                    <div>
+                        <Boton to="/iniciar-sesion">Iniciar Sesión</Boton>
+                    </div>
+                </ContenedorHeader>
+            </Header>
+            <Formulario>
+                <Svg />
+                <Input 
+                    type="email"
+                    name="email"
+                    placeholder="example@mail.com"
+                />
+                <Input 
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                />
+                <Input 
+                    type="password"
+                    name="password2"
+                    placeholder="Repeat password"
+                />
+                <ContenedorBoton>
+                    {/* El argumento de botón "primario"
+                    está definido en Boton.js */}
+                    <Boton as="button" type="submit " primario>Crear Cuenta</Boton>
+                </ContenedorBoton>
+            </Formulario>
+
+        </>
      );
 }
  
---
+++ projects/expense-tracker-app/src/theme.js
@@ -0,0 +1,24 @@
+/* ----------------------------------------------
+    ESTE ARCHIVO CONTIENE LOS COLORES GLOBALES  
+    PARA TODA LA APLICACIION. 
+    ---------------------------------------------
+    Al cambiar un color de elemento, lo va a 
+    actualizar en todos los componentes que lo
+    estén usando y así no tengo que cambiarlo 
+    uno por uno
+----------------------------------------------- */
+
+const theme = {
+    fondo: '#f9f9f9',
+    colorPrimario: '#5b69e2',
+    colorSecundario: '#000',
+    verde: '#43a854',
+    rojo: '#e34747',
+    grisClaro: '#e8eff1',
+    grisClaro2: '#cbdde2',
+    azulClaro: '#8792f1'
+}
+// Para poder usar esta constante debo exportarla
+export default theme; 
+
+
