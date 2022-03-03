cf88012e243fa84495233cbcdd2be9abc1907135
ruedalibre <andresruedalibre@gmail.com>
2022-03-03 13:28:46 -0500

    add expenses code

--- projects/expense-tracker-app/src/componentes/FormularioGasto.js
+++ projects/expense-tracker-app/src/componentes/FormularioGasto.js
@@ -14,6 +14,7 @@ import fromUnixTime from "date-fns/fromUnixTime";
 import getUnixTime from "date-fns/getUnixTime";
 import agregarGasto from "../firebase/agregarGasto";
 import { useAuth } from './../contextos/AuthContext';
+import Alerta from './../elementos/Alerta';
 
 const FormularioGasto = () => {
     /* -------------------------------------------------
@@ -26,6 +27,11 @@ const FormularioGasto = () => {
     const [categoria, cambiarCategoria] = useState('hogar');
     /* Esta función captura la fecha del Date Picker para luego pasarsela a la DB. El valor por defecto de la fecha será al fecha actual, la cual se genera con new Date */
     const [fecha, cambiarFecha] = useState(new Date());
+    /* Agrego los dos estados necesarios para mostrar la alerta del formulario */
+    const [estaodAlerta, cambiarEstadoAlerta] = useState(false);
+    const [alerta, cambiarAlerta] = useState({});
+    
+    
     /* Con este import accedo al usuario que está usando la app */
     const {usuario} = useAuth();
     
@@ -46,18 +52,43 @@ const FormularioGasto = () => {
         /* Para convertir los valores numéricos a float y agregarle dos decimales */
         let cantidad = parseFloat(inputCantidad).toFixed(2);
 
-        console.log(getUnixTime(fecha));
-
-        agregarGasto({
-            /* Aquí creo el objeto con todas sus propiedades */
-            categoria: categoria,
-            descripcion: inputDescripcion,
-            cantidad: inputCantidad,
-            /* La fecha se transorma a formato UnixTime antes de pasarla a la base de datos */
-            fecha: getUnixTime(fecha),
-            /* Para que la app sepa a cuál usuario le debe agregar el objeto, debe tener acceso a su id universal (uid) creado mediante AuthContext ---> useAuth */
-            uidUsuario: usuario.uid
-        });
+        /* Comprobación de que el usuario haya ingresado tanto la descripción como su valor. No debe haber valores vacíos en el formulario */
+        if(inputDescripcion !== '' && inputCantidad !== '') {
+            /* También debo comprobar que la cantidad ingresada sea válida para que el parseFloat la pueda procesar */
+            if(cantidad) {
+                agregarGasto({
+                    /* Aquí creo el objeto con todas sus propiedades */
+                    categoria: categoria,
+                    descripcion: inputDescripcion,
+                    cantidad: inputCantidad,
+                    /* La fecha se transorma a formato UnixTime antes de pasarla a la base de datos */
+                    fecha: getUnixTime(fecha),
+                    /* Para que la app sepa a cuál usuario le debe agregar el objeto, debe tener acceso a su id universal (uid) creado mediante AuthContext ---> useAuth */
+                    uidUsuario: usuario.uid
+                })
+                /* Cuando se valide que todos los valores enviado a al db son correctos, reinicio el formulario a su estado por defecto para seguir agregando elementos */
+                .then(() => {
+                    cambiarCategoria('hogar');
+                    cambiarInputDescripcion('');
+                    cambiarInputCantidad('');
+                    cambiarFecha(new Date());
+                    /* La alerta indica que los datos fueron ingresados correctamente */
+                    cambiarEstadoAlerta(true);
+                    cambiarAlerta({tipo: 'exito', mensaje: 'El gasto fue agregado correctamente'})
+                })
+                /* En caso de que haya un error inesperado al conectarse con la db */
+                .catch((error) => {
+                    cambiarEstadoAlerta(true);
+                    cambiarAlerta({tipo: 'error', mensaje: 'Hubo un problema al intentar agregar tu gasto'})
+                })
+            } else {
+                cambiarEstadoAlerta(true);
+                cambiarAlerta({tipo: 'error', mensaje: 'El valor que ingresaste no es correcto'})
+            }
+        } else {
+            cambiarEstadoAlerta(true);
+            cambiarAlerta({tipo: 'error', mensaje: 'Por favor rellena todos los campos'})
+        }
     }
     
     return ( 
@@ -94,7 +125,14 @@ const FormularioGasto = () => {
                 <Boton as="button" primario conIcono type="submit">
                     Agregar Gasto <IconoPlus />
                 </Boton>
-            </ContenedorBotones>       
+            </ContenedorBotones>
+            {/* La alerta se debe incluir en los elementos del formulario, así este desactivada por defecto */}
+            <Alerta 
+                tipo={alerta.tipo}
+                mensaje={alerta.mensaje}
+                estadoAlerta={estaodAlerta}
+                cambiarEstadoAlerta={cambiarEstadoAlerta}
+            />       
         </Formulario>
      );
 }
