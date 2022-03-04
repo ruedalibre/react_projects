51a5d296f9a0253c43fa791a39f471ad5c982efa
ruedalibre <andresruedalibre@gmail.com>
2022-03-04 12:58:34 -0500

    expense list code

--- projects/expense-tracker-app/src/componentes/ListaDeGastos.js
+++ projects/expense-tracker-app/src/componentes/ListaDeGastos.js
@@ -5,15 +5,23 @@ import BtnRegresar from "../elementos/BtnRegresar";
 import {useAuth} from './../contextos/AuthContext';
 import BarraTotalGastado from "./BarraTotalGastado";
 import useObtenerGastos from "../hooks/useObtenerGastos";
+/* Imports para los estilos de todos los elementos de la lista de gastos */
+import { Lista, ElementoLista, ListaDeCategorias,
+	ElementoListaCategorias, Categoria, Descripcion,
+	Valor, Fecha, ContenedorBotones, BotonAccion,
+	BotonCargarMas, ContenedorBotonCentral,
+	ContenedorSubtitulo, Subtitulo } from "../elementos/ElementosDeLista";
+import IconoCategoria from "../elementos/IconoCategoria";
+import convertirAMoneda from './../funciones/convertirAMoneda';
+import { ReactComponent as IconoEditar} from './../imagenes/editar.svg';
+import { ReactComponent as IconoBorrar} from './../imagenes/borrar.svg';
+import { Link } from "react-router-dom";
+import Boton from './../elementos/Boton';
 
 /* Aquí accedo al contexto global y puedo extraer sus valores true, false y null según sea el caso y usar condicionales para mostrar u ocultar el contenido de la app */
 const ListaDeGastos = () => {   
     const [gastos] = useObtenerGastos();
-    /* El hook debe ejecutarse una sola vez. Para eso uso useEffect. */
-    useEffect(() => {
-
-    }, []);
-    
+        
     return ( 
         <>
             <Helmet>
@@ -24,6 +32,43 @@ const ListaDeGastos = () => {
                 <BtnRegresar />
                 <Titulo>Lista de Gastos</Titulo>
             </Header>
+            <Lista>
+                {/* Itera en la lista de gastos y lista cada categoría con su respectivo ícono */}
+                {gastos.map((gasto) => {
+                    return(
+                        <ElementoLista key={gasto.id}>
+                            <Categoria>
+                                <IconoCategoria id={gasto.categoria} />
+                                {gasto.categoria}
+                            </Categoria>
+                            <Descripcion>
+                                {gasto.descripcion}
+                            </Descripcion>
+                            <Valor>
+                                {convertirAMoneda(gasto.cantidad)}
+                            </Valor>
+                            <ContenedorBotones>
+                                <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
+                                    <IconoEditar/>
+                                </BotonAccion>
+                                <BotonAccion>
+                                    <IconoBorrar/>
+                                </BotonAccion>
+                            </ContenedorBotones>
+                            
+                        </ElementoLista>
+                    )
+                })}
+                <ContenedorBotonCentral>
+                    <BotonCargarMas>Cargar más</BotonCargarMas>
+                </ContenedorBotonCentral>
+                {/* Si la lista de gastos está vacía, muestra este mensaje */}
+                {gastos.length === 0 &&
+                    <ContenedorSubtitulo>
+                        <Subtitulo>No hay gastos por mostrar</Subtitulo>
+                        <Boton as={Link} to="/">Agregar Gasto</Boton>
+                    </ContenedorSubtitulo>}
+            </Lista>
 
             <BarraTotalGastado/>
        </>
