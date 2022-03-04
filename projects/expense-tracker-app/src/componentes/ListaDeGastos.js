31e50745fa09adfa78c9518b709c29b89b9719cb
ruedalibre <andresruedalibre@gmail.com>
2022-03-04 16:34:43 -0500

    adding date to expense list

--- projects/expense-tracker-app/src/componentes/ListaDeGastos.js
+++ projects/expense-tracker-app/src/componentes/ListaDeGastos.js
@@ -17,10 +17,33 @@ import { ReactComponent as IconoEditar} from './../imagenes/editar.svg';
 import { ReactComponent as IconoBorrar} from './../imagenes/borrar.svg';
 import { Link } from "react-router-dom";
 import Boton from './../elementos/Boton';
+/* Import para dar formato a las fechas que entrega la db */
+import {format, fromUnixTime} from 'date-fns';
+/* import para obtener la fecha en español */
+import {es} from 'date-fns/locale';
 
 /* Aquí accedo al contexto global y puedo extraer sus valores true, false y null según sea el caso y usar condicionales para mostrar u ocultar el contenido de la app */
 const ListaDeGastos = () => {   
     const [gastos] = useObtenerGastos();
+
+    /* Para dar formatear a la fecha, pasando de UnixTime a Javascript */
+    const formatearFecha = (fecha) => {
+        return format(fromUnixTime(fecha), "dd 'de' MMMM 'de' yyyy", {locale: es});
+    }
+    /* Para agrupar los gastos por fechas iguales y no repetir la etiqueta con misma feha para cada gasto del mismo día */
+    const fechaEsIgual = (gastos, index, gasto) => {
+        /* Si el index es diferente de 0 significa que ese gasto ya está marcado con su fecha, por lo tanto los que siguen y que tienen el mismo id, también */
+        if(index !== 0) {
+            const fechaActual = formatearFecha(gasto.fecha);
+            const fechaGastoAnterior = formatearFecha(gastos[index -1].fecha);
+
+            if(fechaActual === fechaGastoAnterior){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
         
     return ( 
         <>
@@ -34,30 +57,36 @@ const ListaDeGastos = () => {
             </Header>
             <Lista>
                 {/* Itera en la lista de gastos y lista cada categoría con su respectivo ícono */}
-                {gastos.map((gasto) => {
+                {gastos.map((gasto, index) => {
                     return(
-                        <ElementoLista key={gasto.id}>
-                            <Categoria>
-                                <IconoCategoria id={gasto.categoria} />
-                                {gasto.categoria}
-                            </Categoria>
-                            <Descripcion>
-                                {gasto.descripcion}
-                            </Descripcion>
-                            <Valor>
-                                {convertirAMoneda(gasto.cantidad)}
-                            </Valor>
-                            <ContenedorBotones>
-                                <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
-                                    <IconoEditar/>
-                                </BotonAccion>
-                                <BotonAccion>
-                                    <IconoBorrar/>
-                                </BotonAccion>
-                            </ContenedorBotones>
-                            
-                        </ElementoLista>
-                    )
+                        <div key={gasto.id}>
+                            {!fechaEsIgual(gastos, index, gasto) && <Fecha>{formatearFecha(gasto.fecha)}</Fecha>}
+                            {/* La fecha va por fuera del elemento lista porque agrupa los gasto por fecha */}
+                            <ElementoLista key={gasto.id}>
+                                <Categoria>
+                                    <IconoCategoria id={gasto.categoria} />
+                                    {gasto.categoria}
+                                </Categoria>
+                                
+                                <Descripcion>
+                                    {gasto.descripcion}
+                                </Descripcion>
+                                
+                                <Valor>
+                                    {convertirAMoneda(gasto.cantidad)}
+                                </Valor>
+                                
+                                <ContenedorBotones>
+                                    <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
+                                        <IconoEditar/>
+                                    </BotonAccion>
+                                    <BotonAccion>
+                                        <IconoBorrar/>
+                                    </BotonAccion>
+                                </ContenedorBotones>
+                            </ElementoLista>
+                        </div>
+                    );
                 })}
                 <ContenedorBotonCentral>
                     <BotonCargarMas>Cargar más</BotonCargarMas>
