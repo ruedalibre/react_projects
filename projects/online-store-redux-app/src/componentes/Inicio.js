71d7080718d0bbdf80f3650ad615134e6f76e6dd
ruedalibre <andresruedalibre@gmail.com>
2022-04-04 15:45:46 -0500

    update all react projects

---
+++ projects/online-store-redux-app/src/componentes/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inicio = () => {
+	return (
+		<div>
+			<h1>Inicio</h1>
+			<p>Modelo base para la creación de un tienda online que contiene un blog y un carrito de compras donde se pueden agregar productos, los cuales ingresan a la lista de comprars. La funcionalidad también incluye la posibilidad de agregar varios producto, los cuales se van sumando a la lista de compras al repetir click sobre el mismo producto. </p>
+		</div>
+	);
+}
+ 
+export default Inicio;
